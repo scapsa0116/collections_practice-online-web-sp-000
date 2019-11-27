52e0cd7a01b1487014ab19918b843e972145e835
Stela Capsa <stela.capsa@gmail.com>
2019-11-27 15:29:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
+def sort_array_asc(n)
+n.sort do |a, b|
+  a <=> b
+end
+end
+
+
+def sort_array_desc(array)
+array.sort do |a, b |
+  b <=> a
+end
+end
+
+
+def sort_array_char_count(ary)
+  ary.sort do |a, b|
+  a[0] <=> b[1]
+end
+end
+
+def swap_elements(element)
+  element.sort do |a, b|
+    a[1] <=> b[2]
+  end
+end
+
+
+def reverse_array(num)
+num.reverse do |a, b|
+  a[0] <=> b[2]
+end
+  end
+  
+def kesha_maker(word)
+  
+word.each{|words| words[2].sub!("$")}
+
+ end
+
+
+def sum_array(w)
+  w.inject(0, :+)
+end
+
+def find_a(text)
+  text.start_with?("a")  
+  
+end
+
+def add_s(elements)
+  [0,2,3].each_with_index.collect{|element, index| element +"s"  }
+end
+
+  
+  
+  
+  
+  
+
 
