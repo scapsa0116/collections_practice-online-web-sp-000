15c9f3a9d44183762ea467cd504c5423937accb2
Stela Capsa <stela.capsa@gmail.com>
2019-11-27 15:55:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,7 +53,7 @@ def find_a(text)
 end
 
 def add_s(elements)
-  [0,2,3].each_with_index.collect{|element, index| element +"s"  }
+  elements.each_with_index.collect{|element, index| element +"s"  }
 end
 
   
